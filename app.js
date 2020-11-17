92dd2a8df417f60d1562752f00d61dbe009aa572
milanzagorac <zagoracm@gmail.com>
2020-11-17 18:44:54 -0500

    bugs

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let newPass = '';
 // function to create a new password - iterates over the chars string
 // and selects random chars based on the desired password length
 function newPassword(passLength) {
-    
     let chars = 'qwertyuiop[]QWERTYUIOP{}|asdfghjkl;ASDFGHJKL:zxcvbnm,./ZXCVBNM<>?1234567890-=!@#$%^&*()_+'; 
     for (let i = 0; i < passLength; i++) {
         newPass += chars.charAt(Math.floor(Math.random() * chars.length))
@@ -22,7 +21,7 @@ generateButton.addEventListener('click', function(ev) {
     
     //get the integer value of the length selection on the page
     let passLength = parseInt(document.getElementById('inputvalue').value);
-    if (passLength < 8 || passLength > 32 || passLength == null) {
+    if (passLength < 8 || passLength > 32) {
         alert('Please enter a valid number within the specified range (8-32)');
     } else {
     
